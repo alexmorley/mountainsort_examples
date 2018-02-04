a4dceceecdc36ebc7da8c86c7930237043660564
Alexander Morley <alexmorley@users.noreply.github.com>
2018-02-04 16:26:29 +0000

    Decrease duration for testing on small (low-RAM) containers

--- devel/accuracy_test_1/run_test.sh
+++ devel/accuracy_test_1/run_test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mlp-run synthesize_v1.mlp synthesize --samplerate=30000 --duration=600 --timeseries=data/raw.mda --geom=data/geom.csv --waveforms_true=data/waveforms_true.mda --firings_true=data/firings_true.mda --num_channels=10 --num_units=50
+mlp-run synthesize_v1.mlp synthesize --samplerate=30000 --duration=60 --timeseries=data/raw.mda --geom=data/geom.csv --waveforms_true=data/waveforms_true.mda --firings_true=data/firings_true.mda --num_channels=10 --num_units=50
 
 mlp-run mountainsort3.mlp sort --raw=data/raw.mda --geom=data/geom.csv --firings_out=data/firings.mda --_params=params.json --curate=true
 
